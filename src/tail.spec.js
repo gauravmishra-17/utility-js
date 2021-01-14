98a0e268ec2385889e882746b9f958b27d36d167
Gaurav Mishra <gaurav.mishra17lko@gmail.com>
2021-01-15 02:48:28 +0530

    test for tail function added

--- src/tail.spec.js
+++ src/tail.spec.js
@@ -4,4 +4,7 @@ describe("Tail", function () {
   it("Tail of [] should be []", () => {
     expect(tail([])).toEqual([]);
   });
+  it("Tail of [1,2,3] should be [2,3]", () => {
+    expect(tail([1, 2, 3])).toEqual([2, 3]);
+  });
 });

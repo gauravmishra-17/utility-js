6487cbda85d01c64b9acf10f5b815e6ebd2260be
Gaurav Mishra <gaurav.mishra17lko@gmail.com>
2021-01-15 03:27:01 +0530

    identity function test in map

--- src/map.spec.js
+++ src/map.spec.js
@@ -16,11 +16,4 @@ describe("Map", () => {
   it("Identity function on [1,2,3] should give [1,2,3]", () => {
     expect(map([1, 2, 3], identity)).toEqual([1, 2, 3]);
   });
-
-  it("Cube function on [1,2,3] should give [1,8,27]", () => {
-    expect(map([1, 2, 3], cube)).toEqual([1, 8, 27]);
-  });
-  it("Testing explicit increment for an object", () => {
-    expect(map([{ x: 10 }], (someObject) => someObject.x + 1)).toEqual([11]);
-  });
 });
